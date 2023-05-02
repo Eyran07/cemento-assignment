d9cb95b6dcd3a976d8c2996dade24f82419e5460
Eyran07 <eyranabbou20@gmail.com>
2023-05-02 20:23:40 +0300

    Home page added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,21 @@
 import Table from "../components/Table";
-import { tableData } from '../data/tableData';
+import { tableData } from "../data/tableData";
 
 const Home = () => {
-    return (
-        <div>
-        <h1>Home</h1>
-        <Table data={tableData} />
-        </div>
-    );
-}
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "80vh",
+      }}
+    >
+      <h1 style={{ marginBottom: "2rem" }}>Cemento Assignment</h1>
+      <Table data={tableData}/>
+    </div>
+  );
+};
 
 export default Home;
